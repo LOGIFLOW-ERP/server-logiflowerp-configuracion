80e697a17fa0203d2f2592f24683a30ec1d903f4
usuario0888 <antonio.mejia@logiflowerp.com>
2025-02-16 22:22:29 -0500

    Fix file path case sensitivity for registration email template

--- src/Context/Shared/Application/UseCaseSendMailRegisterUser.ts
+++ src/Context/Shared/Application/UseCaseSendMailRegisterUser.ts
@@ -30,7 +30,7 @@ export class UseCaseSendMailRegisterUser {
     }
 
     private prepareHTMLmessage(token: string) {
-        const filePath = path.join(__dirname, '../../../../public/registerUser.html')
+        const filePath = path.join(__dirname, '../../../../public/RegisterUser.html')
         const html = fs.readFileSync(filePath, 'utf-8')
             .replace('{ENLACE_ACTIVACION}', `http://google.com?token=${token}`)
         return html
