3288cf8a3641bc95bae8e3af782a88fec66e5e55
usuario0888 <antonio.mejia@logiflowerp.com>
2025-02-21 06:19:11 -0500

    Refactor user signup process to handle missing DNI data and improve data validation

--- src/Context/Processes/Auth/Application/UseCaseSignUp.ts
+++ src/Context/Processes/Auth/Application/UseCaseSignUp.ts
@@ -3,7 +3,7 @@ import { IRENIECPersonalData, IUserMongoRepository } from '@Masters/User/Domain'
 import { USER_TYPES } from '@Masters/User/Infrastructure'
 import { AdapterApiRequest, SHARED_TYPES } from '@Shared/Infrastructure'
 import { inject, injectable } from 'inversify'
-import { CreateUserDTO, UserENTITY, validateCustom } from 'logiflowerp-sdk'
+import { CreateUserDTO, DocumentType, UserENTITY, validateCustom } from 'logiflowerp-sdk'
 
 @injectable()
 export class UseCaseSignUp {
@@ -14,25 +14,28 @@ export class UseCaseSignUp {
 	) { }
 
 	async exec(dto: CreateUserDTO) {
-		const RENIECPersonalData = await this.RENIECPersonalDataConsultation(dto.identity)
+		const RENIECPersonalData = await this.RENIECPersonalDataConsultation(dto)
 		const entity = this.completeUserData(dto, RENIECPersonalData)
 		const validatedEntity = await validateCustom(entity, UserENTITY, UnprocessableEntityException)
 		return await this.repository.insertOne(validatedEntity)
 	}
 
-	private async RENIECPersonalDataConsultation(dni: string) {
-		const url = `${env.DNI_LOOKUP_API_URL}/v2/reniec/dni?numero=${dni}`
+	private async RENIECPersonalDataConsultation(dto: CreateUserDTO) {
+		if (dto.documentType !== DocumentType.DNI) return
+		const url = `${env.DNI_LOOKUP_API_URL}/v2/reniec/dni?numero=${dto.identity}`
 		const headers = { Authorization: `Bearer ${env.DNI_LOOKUP_API_TOKEN}` }
 		const result = await this.adapterApiRequest.get<IRENIECPersonalData>(url, { headers })
 		return await validateCustom(result, IRENIECPersonalData, UnprocessableEntityException)
 	}
 
-	private completeUserData(dto: CreateUserDTO, RENIECPersonalData: IRENIECPersonalData) {
+	private completeUserData(dto: CreateUserDTO, RENIECPersonalData: IRENIECPersonalData | undefined) {
 		const newUser = new UserENTITY()
 		newUser._id = crypto.randomUUID()
 		newUser.set(dto)
-		newUser.names = RENIECPersonalData.nombres
-		newUser.surnames = `${RENIECPersonalData.apellidoPaterno} ${RENIECPersonalData.apellidoMaterno}`
+		if (RENIECPersonalData) {
+			newUser.names = RENIECPersonalData.nombres
+			newUser.surnames = `${RENIECPersonalData.apellidoPaterno} ${RENIECPersonalData.apellidoMaterno}`
+		}
 		return newUser
 	}
 
