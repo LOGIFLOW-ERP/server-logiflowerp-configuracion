486ebfd9090c37b7a4287c864399babfb3ebf564
usuario0888 <antonio.mejia@logiflowerp.com>
2025-03-12 21:02:11 -0500

    Refactor Bootstrap classes to accept root companies as parameters; update logging for collection configuration; modify deleteMany and insertMany operations to reflect accurate operation types.

--- src/Config/collections.ts
+++ src/Config/collections.ts
@@ -1,12 +1,21 @@
 import { Dirent, globSync } from 'fs'
 import path from 'path'
+import { RootCompanyMongoRepository } from '@Masters/RootCompany/Infrastructure/MongoRepository'
+import { prefix_col_root, State } from 'logiflowerp-sdk'
+
+async function getRootCompanies() {
+    const repository = new RootCompanyMongoRepository(prefix_col_root)
+    const pipeline = [{ $match: { state: State.ACTIVO } }]
+    return repository.select(pipeline)
+}
 
 async function initcollection(paths: Dirent[]) {
+    const rootCompanies = await getRootCompanies()
     for (let rute of paths) {
         const newPath = path.join('../', `${rute.parentPath.split('src')[1]}/${rute.name}`)
         const { ManagerEntity } = await import(newPath)
         const app = new ManagerEntity()
-        await app.exec()
+        await app.exec(rootCompanies)
     }
 }
 
