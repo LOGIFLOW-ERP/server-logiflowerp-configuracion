ac155fffaa8cf56dda026c18fa38e291fc902f22
usuario0888 <antonio.mejia@logiflowerp.com>
2025-08-19 21:21:57 -0500

    refactor: comment out unused execRoot function and its invocation

--- src/Config/registerRoutes.ts
+++ src/Config/registerRoutes.ts
@@ -11,24 +11,24 @@ export async function registerRoutes(rootPath: string) {
     try {
         const repositoryMongoSystemOption = new MongoRepository<SystemOptionENTITY>(db_root, collections.systemOption, new AuthUserDTO())
 
-        const execRoot = async (rawData: RouteInfo[], rootPath: string, prefix: string) => {
-            const dataDB = await repositoryMongoSystemOption.select([{ $match: { prefix, root: true } }])
-            const rawDataAux = rawData.filter(e => e.controller.startsWith('Root'))
-            const { _ids, newData } = await builSystemOption({
-                dataDB,
-                prefix,
-                rawData: rawDataAux,
-                rootPath,
-                UnprocessableEntityException,
-                root: true
-            })
-            if (_ids.length) {
-                await repositoryMongoSystemOption.deleteMany({ _id: { $in: _ids } })
-            }
-            if (newData.length) {
-                await repositoryMongoSystemOption.insertMany(newData)
-            }
-        }
+        // const execRoot = async (rawData: RouteInfo[], rootPath: string, prefix: string) => {
+        //     const dataDB = await repositoryMongoSystemOption.select([{ $match: { prefix, root: true } }])
+        //     const rawDataAux = rawData.filter(e => e.controller.startsWith('Root'))
+        //     const { _ids, newData } = await builSystemOption({
+        //         dataDB,
+        //         prefix,
+        //         rawData: rawDataAux,
+        //         rootPath,
+        //         UnprocessableEntityException,
+        //         root: true
+        //     })
+        //     if (_ids.length) {
+        //         await repositoryMongoSystemOption.deleteMany({ _id: { $in: _ids } })
+        //     }
+        //     if (newData.length) {
+        //         await repositoryMongoSystemOption.insertMany(newData)
+        //     }
+        // }
 
         const execNoRoot = async (rawData: RouteInfo[], rootPath: string, prefix: string) => {
             const dataDB = await repositoryMongoSystemOption.select([{ $match: { prefix, root: false } }])
@@ -48,7 +48,7 @@ export async function registerRoutes(rootPath: string) {
             }
         }
 
-        await execRoot(routes, rootPath, env.PREFIX)
+        // await execRoot(routes, rootPath, env.PREFIX)
         await execNoRoot(routes, rootPath, env.PREFIX)
         console.log(styleText('yellow', '>>> Routes registered successfully.'))
     } catch (error) {
