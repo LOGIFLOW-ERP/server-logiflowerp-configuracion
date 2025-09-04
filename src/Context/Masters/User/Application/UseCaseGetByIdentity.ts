4a44123d3b2a5719ca68bd4e317c2552db394f89
usuario0888 <antonio.mejia@logiflowerp.com>
2025-09-04 18:52:04 -0500

    mejora msj

--- src/Context/Masters/User/Application/UseCaseGetByIdentity.ts
+++ src/Context/Masters/User/Application/UseCaseGetByIdentity.ts
@@ -2,6 +2,7 @@ import { State } from 'logiflowerp-sdk'
 import { IUserMongoRepository } from '../Domain'
 import { inject, injectable } from 'inversify'
 import { USER_TYPES } from '../Infrastructure/IoC/types'
+import { NotFoundException } from '@Config/exception'
 
 @injectable()
 export class UseCaseGetByIdentity {
@@ -12,8 +13,14 @@ export class UseCaseGetByIdentity {
 
 	async exec(identity: string) {
 		const pipeline = [{ $match: { identity, state: State.ACTIVO, isDeleted: false } }]
-		const result = await this.repository.selectOne(pipeline)
-		const { password, root, emailVerified, state, _id, ...user } = result
+		const result = await this.repository.select(pipeline)
+		if (!result.length) {
+			throw new NotFoundException('Usuario con identidad ' + identity + ' no tiene una cuenta.', true)
+		}
+		if (result[0].state === State.INACTIVO) {
+			throw new NotFoundException('Usuario con identidad ' + identity + ' tiene una cuenta inactiva.', true)
+		}
+		const { password, root, emailVerified, state, _id, ...user } = result[0]
 		return user
 	}
 
