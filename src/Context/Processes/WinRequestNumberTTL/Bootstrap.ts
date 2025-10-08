82329c8f44dd893f08907440ce244c7ae844c12e
usuario0888 <antonio.mejia@logiflowerp.com>
2025-10-07 21:45:40 -0500

    update logiflowerp-sdk to version 1.0.244 and nodemailer to version 7.0.9; add ManagerEntity class for request number TTL management

---
+++ src/Context/Processes/WinRequestNumberTTL/Bootstrap.ts
@@ -0,0 +1,43 @@
+import { IndexEntity } from '@Shared/Domain'
+import { collection } from './Infrastructure/config'
+import { Bootstraping } from '@Shared/Bootstraping'
+import { RequestNumberTTLENTITY, RootCompanyENTITY } from 'logiflowerp-sdk'
+import { inject, injectable } from 'inversify'
+import { SHARED_TYPES } from '@Shared/Infrastructure'
+
+@injectable()
+export class ManagerEntity {
+
+    private indexes: IndexEntity<RequestNumberTTLENTITY>[] = [
+        {
+            campos: { numero_de_peticion: 1 },
+            opciones: {
+                name: 'idx_numero_de_peticion',
+                unique: true
+            }
+        },
+        {
+            campos: { createdAt: 1 },
+            opciones: {
+                name: 'idx_createdAt',
+                expireAfterSeconds: 60 * 60 * 24 * 90 // 90 dias
+            }
+        }
+    ]
+
+    constructor(
+        @inject(SHARED_TYPES.Bootstraping) private bootstrap: Bootstraping
+    ) { }
+
+    async exec(rootCompanies: RootCompanyENTITY[]) {
+        for (const company of rootCompanies) {
+            const db = company.code
+            const col = collection
+
+            console.info(`➽  Configurando ${col} en ${db} ...`)
+            await this.bootstrap.exec(db, col, this.indexes)
+            console.info(`➽  Configuración de ${col} en ${db} completada`)
+        }
+    }
+
+}
